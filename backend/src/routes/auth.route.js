f8cc75bac5d8daaea3b29a13be5e734d77028c62
anshIA09 <Anish.Pawaskar@interactiveavenues.com>
2026-03-12 16:07:03 +0530

    feat: add auth routes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import {
+  signinController,
+  signupController,
+} from "../controllers/auth.controller.js";
 
 const router = Router();
+
+router.post("/signup", signupController);
+router.post("/signin", signinController);
 
 export default router;
