66dfcf46a1e779cf75a70e36dd6d77c798e604fa
anshIA09 <Anish.Pawaskar@interactiveavenues.com>
2026-03-12 14:38:43 +0530

    chore: initialize frontend app

---
+++ frontend/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-background text-foreground">
+      <h1 className="text-4xl font-bold">Hello world</h1>
+    </div>
+  );
+}
